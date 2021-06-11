3f16783dbee3e41e8a1ed1ca79b508953e5e7c14
Luca Giozza <luca.giozza@gmail.com>
2021-06-11 15:19:27 +0200

    inizio prova esercizio

--- js/script.js
+++ js/script.js
@@ -4,3 +4,38 @@
 // i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti
 //  e quali dei numeri da indovinare sono stati individuati.
+
+
+var numeriRandom = []
+for(var i = 0; i < 5 ; i++){
+    var casuali = Math.floor(Math.random() * 100) ;
+    numeriRandom.push(casuali);
+    
+}
+alert(' Attento! Tra poco verranno generati 5 numeri; quando pensi di averli memorizzati premi su ok! Poi partirà un timer di 30 secondi e successivamente dovrai inserire il maggior numero possibile di numeri che ti ricordi! Quando sei pronto premi ok ')
+alert(numeriRandom);
+
+
+var tempo = 30;
+
+var scadenza = setInterval(timer, 1000);
+
+function timer(){
+    if(!tempo){
+        clearInterval(scadenza);
+        alert('fine timer')
+
+    }else{
+        
+        console.log(--tempo + 1) 
+       
+    }
+
+    
+     
+}
+
+for(var i = 0; i < 5 ; i++){
+    
+    var inserimento = parseInt(prompt('Ora inserisci, uno alla volta, tutti i numeri che ti ricordi'))
+}
